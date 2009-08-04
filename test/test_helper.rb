59d86f57b90115443ce6965184932e25c11136ec
Lance Ivy <lance@cainlevy.net>
2009-08-04 14:00:53 -0700

    updating tests to run vs rails 2.3.2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,33 +1,28 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 RAILS_ROOT = File.dirname(__FILE__)
+ENV["RAILS_ENV"] = "test"
 
 require 'rubygems'
 require 'test/unit'
 require 'active_record'
 require 'active_record/fixtures'
-require 'active_support/binding_of_caller'
-require 'active_support/breakpoint'
 require "#{File.dirname(__FILE__)}/../init"
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'plugin_test'])
 
+ActiveRecord::Migration.verbose = false
 load(File.dirname(__FILE__) + "/schema.rb") if File.exist?(File.dirname(__FILE__) + "/schema.rb")
 
-Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) + "/fixtures/"
-$LOAD_PATH.unshift(Test::Unit::TestCase.fixture_path)
+$LOAD_PATH.unshift(File.dirname(__FILE__) + "/fixtures/")
 
-class Test::Unit::TestCase #:nodoc:
-  def create_fixtures(*table_names)
-    if block_given?
-      Fixtures.create_fixtures(Test::Unit::TestCase.fixture_path, table_names) { yield }
-    else
-      Fixtures.create_fixtures(Test::Unit::TestCase.fixture_path, table_names)
-    end
-  end
-
-  self.use_transactional_fixtures = true
+class ActiveSupport::TestCase #:nodoc:
+  include ActiveRecord::TestFixtures
   
+  self.fixture_path = File.dirname(__FILE__) + '/fixtures/'
+  self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
+  
+  fixtures :all
 end
