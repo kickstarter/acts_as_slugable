edd9849e7132e0466a33634efbb7077cb5aa14c1
Lance Ivy <lance@cainlevy.net>
2010-04-02 13:48:27 -0700

    removing ^M from line endings

--- test/acts_as_slugable_test.rb
+++ test/acts_as_slugable_test.rb
@@ -20,7 +20,7 @@ class ActsAsSlugableTest < ActiveSupport::TestCase
     assert pg.valid?
     assert_equal "something-different", pg.url_slug
   end
-  
+
   def test_model_still_runs_validations
     # create, with nil title
     pg = Page.create(:title => nil)
@@ -45,23 +45,23 @@ class ActsAsSlugableTest < ActiveSupport::TestCase
     # update, with title and nil slug
     pg.update_attributes(:title => "Updated title and slug to nil", :url_slug => nil)
     assert_equal "updated-title-and-slug-to-nil", pg.url_slug
-    
+
     # update, with empty slug
     pg.update_attributes(:title => "Updated title and slug to empty", :url_slug => '')
     assert_equal "updated-title-and-slug-to-empty", pg.url_slug
   end
 
   def test_uniqueness
-    # create two pages with the same title and 
+    # create two pages with the same title and
     # within the same scope - slugs should be unique
     t = "Unique title"
 
     pg1 = Page.create(:title => t, :parent_id => 1)
     assert pg1.valid?
-    
+
     pg2 = Page.create(:title => t, :parent_id => 1)
     assert pg2.valid?
-    
+
     assert_not_equal pg1.url_slug, pg2.url_slug
   end
 
@@ -72,7 +72,7 @@ class ActsAsSlugableTest < ActiveSupport::TestCase
 
     pg1 = Page.create(:title => t, :parent_id => 1)
     assert pg1.valid?
-    
+
     pg2 = Page.create(:title => t, :parent_id => 2)
     assert pg2.valid?
 
@@ -101,11 +101,11 @@ class StringToSlugTest < ActiveSupport::TestCase
   def test_converting_ampersands
     assert_equal "test-and-test-again", "Test & test again".to_slug
   end
-  
+
   def test_converting_ampersands_in_long_strings
     assert_equal "aaa-and-b", "aaa & bbb".to_slug(:length => 9)
   end
-  
+
   def test_sandwiched_punctuation
     assert_equal 'test', "!Test!".to_slug
   end
@@ -121,6 +121,6 @@ class StringToSlugTest < ActiveSupport::TestCase
 
   private
     def check_for_allowable_characters(str)
-      assert_match @allowable_characters, str.to_slug  
+      assert_match @allowable_characters, str.to_slug
     end
 end
