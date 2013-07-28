998ddc0b54d56947a4a031a26996bf2eec08ef3d
Lance Ivy <lance@cainlevy.net>
2013-07-28 12:56:04 -0400

    remove old plugin init.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 require 'test/unit'
 require 'active_record'
 require 'active_record/fixtures'
-require "#{File.dirname(__FILE__)}/../init"
+require_relative '../lib/acts_as_sluggable'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
