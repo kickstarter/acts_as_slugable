70717fce51ba85a9a6bbefbbdd892d18fd2bb1cb
Lance Ivy <lance@cainlevy.net>
2013-07-28 13:37:15 -0400

    simplify metaprogramming

--- lib/acts_as_sluggable.rb
+++ lib/acts_as_sluggable.rb
@@ -5,7 +5,7 @@ module ActsAsSluggable
     @defaults ||= {
       :source_column => 'name',
       :slug_column => 'slug',
-      :scope => nil,
+      :scope => "1 = 1",
       :slug_length => 50
     }
   end
@@ -31,75 +31,58 @@ module ActsAsSluggable
       gsub(/[-\s]*$/, '')
   end
 
-  def self.append_features(base)
-    super
-    base.extend(ClassMethods)
-  end
 
-  module ClassMethods
-    # Generates a URL slug based on provided fields and adds <tt>after_validation</tt> callbacks.
-    #
-    #   class Page < ActiveRecord::Base
-    #     acts_as_sluggable :source_column => :title, :slug_column => :slug, :scope => :parent
-    #   end
-    #
-    # Configuration options:
-    # * <tt>source_column</tt> - specifies the column name used to generate the URL slug
-    # * <tt>slug_column</tt> - specifies the column name used to store the URL slug
-    # * <tt>scope</tt> - Given a symbol, it'll attach "_id" and use that as the foreign key
-    #   restriction. It's also possible to give it an entire string that is interpolated if
-    #   you need a tighter scope than just a foreign key.
-    # * <tt>slug_length</tt> - specifies a maximum length for slugs, before any unique suffix is added.
-    def acts_as_sluggable(options = {})
-      configuration = ActsAsSluggable.defaults.merge(options)
+  # Generates a URL slug based on provided fields and adds <tt>after_validation</tt> callbacks.
+  #
+  #   class Page < ActiveRecord::Base
+  #     acts_as_sluggable :source_column => :title, :slug_column => :slug, :scope => :parent
+  #   end
+  #
+  # Configuration options:
+  # * <tt>source_column</tt> - specifies the column name used to generate the URL slug
+  # * <tt>slug_column</tt> - specifies the column name used to store the URL slug
+  # * <tt>scope</tt> - Given a symbol, it'll attach "_id" and use that as the foreign key
+  #   restriction. It's also possible to give it an entire string that is interpolated if
+  #   you need a tighter scope than just a foreign key.
+  # * <tt>slug_length</tt> - specifies a maximum length for slugs, before any unique suffix is added.
+  def acts_as_sluggable(options = {})
+    configuration = ActsAsSluggable.defaults.merge(options)
+    configuration[:scope] = "#{configuration[:scope]}_id".intern if configuration[:scope].is_a?(Symbol) && configuration[:scope].to_s !~ /_id$/
 
-      configuration[:scope] = "#{configuration[:scope]}_id".intern if configuration[:scope].is_a?(Symbol) && configuration[:scope].to_s !~ /_id$/
-
-      if configuration[:scope].is_a?(Symbol)
-        scope_condition_method = %(
-          def slug_scope_condition
-            if #{configuration[:scope].to_s}.nil?
-              "#{configuration[:scope].to_s} IS NULL"
-            else
-              "#{configuration[:scope].to_s} = \#{#{configuration[:scope].to_s}}"
-            end
-          end
-        )
-      elsif configuration[:scope].nil?
-        scope_condition_method = "def slug_scope_condition() \"1 = 1\" end"
-      else
-        scope_condition_method = "def slug_scope_condition() \"#{configuration[:scope]}\" end"
-      end
-
-      class_eval <<-EOV
-        include ActsAsSluggable::InstanceMethods
-
-        def acts_as_sluggable_class
-          ::#{self.name}
-        end
-
-        def source_column
-          "#{configuration[:source_column]}"
-        end
-
-        def slug_column
-          "#{configuration[:slug_column]}"
-        end
-
-        def slug_length
-          #{configuration[:slug_length]}
-        end
-
-        #{scope_condition_method}
-
-        after_validation :create_slug
-      EOV
+    define_method(:acts_as_sluggable_config) do
+      configuration
     end
+
+    include ActsAsSluggable::InstanceMethods
+
+    after_validation :create_slug
   end
 
-  # Adds instance methods.
   module InstanceMethods
+    def source_column
+      acts_as_sluggable_config[:source_column]
+    end
+
+    def slug_column
+      acts_as_sluggable_config[:slug_column]
+    end
+
+    def slug_length
+      acts_as_sluggable_config[:slug_length]
+    end
+
+    def slug_scope_condition
+      scope = acts_as_sluggable_config[:scope]
+
+      if scope.is_a?(Symbol)
+        {scope => send(scope)}
+      else
+        scope
+      end
+    end
+
     private
+
       # URL slug creation logic
       #
       # The steps are roughly as follows
@@ -120,10 +103,10 @@ module ActsAsSluggable
 
           suffix = ""
           existing = true
-          acts_as_sluggable_class.transaction do
+          transaction do
             while existing != nil
               # look for records with the same url slug and increment a counter until we find a unique slug
-              existing = acts_as_sluggable_class.
+              existing = self.class.
                 where(slug_column => proposed_slug + suffix).
                 where(slug_scope_condition).first
               if existing
@@ -137,4 +120,4 @@ module ActsAsSluggable
   end
 end
 
-::ActiveRecord::Base.send :include, ActsAsSluggable
+::ActiveRecord::Base.send :extend, ActsAsSluggable
